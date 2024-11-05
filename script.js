dac30322a10cadb8f61dce29f46497a218fd250d
ifmiska23 <mihalyabraham23@gmail.com>
2024-11-05 13:33:27 +0100

    masodik ütem

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let elozo = document.createElement("button");
 elozo.innerText = "Előző hónap";
+
 let ido = document.createElement("span");
 ido.innerText = "2024. 11. hónap";
+
 let kovetkezo = document.createElement("button");
 kovetkezo.innerText = "Következő hónap";
+
 const fejlec = document.getElementById("fejlec");
 let naptar = document.getElementById("naptar");
+
 fejlec.appendChild(elozo)
 fejlec.appendChild(ido)
 fejlec.appendChild(kovetkezo)
@@ -18,5 +22,4 @@ for (let i = 1; i <31 ; i++){
         sor.appendChild(nap);
     }
     naptar.appendChild(sor);
-
 }
